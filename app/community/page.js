36fdc638bfcb6862e17de6b60bb4093d0fb13af4
65070129 <65070129@kmitl.ac.th>
2025-02-21 18:22:45 +0700

    i don't know

--- app/community/page.js
+++ app/community/page.js
@@ -3,31 +3,8 @@ import React from 'react'
 import Navbar from '@/components/Navbar';
 import styles from './community.module.css'
 import Image from 'next/image';
-import { useState, useEffect } from 'react';
 
 export default function Community() {
-
-    const [username, setUsername] = useState('');
-
-
-    useEffect(() => {
-        const storedUser = localStorage.getItem('user');
-        const sessionUser = sessionStorage.getItem('user');
-
-        if (storedUser) {
-            const user = JSON.parse(storedUser);
-            if (user && user.username) {
-                setUsername(user.username);
-            }
-        } else if (sessionUser) {
-            const user = JSON.parse(sessionUser);
-            if (user && user.username) {
-                setUsername(user.username);
-            }
-        }
-    }, []);
-
-
     return (
         <main>
             <Navbar />
@@ -40,7 +17,6 @@ export default function Community() {
                         height={50}
                         className={styles.iconProfile}
                     />
-                    <p>{username}</p>
                 </div>
             </div>
         </main>
